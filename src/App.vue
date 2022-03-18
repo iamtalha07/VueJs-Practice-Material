15d1ab0ded8c1501905c4af50d7f4ebb390d3450
talh456 <talha.ishaq@arpatech.com>
2022-03-18 10:42:06 +0500

    Named slots

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,10 @@
       <app-article></app-article>
       <hr>
       <app-contact>
-        
+        <!-- Default slot -->
+        <p>Some content</p> 
+        <p slot="top">Contact us and one of our representative will get back to you as soon as possible.</p>
+        <p slot="bottom">Please do not share any sensitive information.</p>
       </app-contact>
     </div>
   </div>
--- src/Contact.vue
+++ src/Contact.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h2>Contact Us</h2>
-
+        <slot name="top"></slot>
         <form>
             <div class="form-group">
                 <label for="yourName">Your Name</label>
@@ -11,12 +11,13 @@
                 <label for="emailAddress">E-mail address</label>
                 <input type="text" class="form-control" id="emailAddress" placeholder="Enter your email address">
             </div>
+            <slot></slot>
             <div class="form-group">
                 <label for="message">Message</label>
                 <textarea id="message" cols="30" rows="10" class="form-control" placeholder="Enter your message"></textarea>
             </div>
             
-            <slot><p>We will respond within 2 business days.</p></slot>
+            <slot name="bottom"><p>We will respond within 2 business days.</p></slot>
 
             <button type="submit" class="btn btn-primary">Send</button>
         </form>
