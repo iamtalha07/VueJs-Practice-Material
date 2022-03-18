4602e01c4e02dcb1742218dacfe0b2ea5a58ba45
talh456 <talha.ishaq@arpatech.com>
2022-03-18 12:38:23 +0500

    Dynamic componenets

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,40 @@
 <template>
-<div class="container">
-  <div class="row">
-    <div class="col-12">
-      <announcement></announcement>
-      <app-article></app-article>
-      <hr>
-      <app-contact>
-        <!-- Default slot -->
-        <p>Some content</p> 
-        <p slot="top">Contact us and one of our representative will get back to you as soon as possible.</p>
-        <p slot="bottom">Please do not share any sensitive information.</p>
-      </app-contact>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#" @click.prevent="activeView='app-home'">Project Name</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#" @click.prevent="activeView='app-home'">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" @click.prevent="activeView='app-about'">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" @click.prevent="activeView='app-contact'">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      <div class="jumbotron">
+        <component :is="activeView"></component>
+      </div>
     </div>
+    
   </div>
-</div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      activeView: 'app-home'
+    }
+  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import BootstrapVue from "bootstrap-vue"
 import App from './App.vue'
-import Announcement from './Announcement.vue'
+import About from './About.vue'
 import Contact from './Contact.vue'
-import Article from './Article.vue'
+import Home from './Home.vue'
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
@@ -11,9 +12,9 @@ Vue.use(BootstrapVue)
 
 export const eventBus = new Vue();
 
-Vue.component('announcement',Announcement)
-Vue.component('appArticle',Article)
-Vue.component('appContact',Contact)
+Vue.component('app-about',About)
+Vue.component('app-home',Home)
+Vue.component('app-contact',Contact)
 
 new Vue({
   el: '#app',
